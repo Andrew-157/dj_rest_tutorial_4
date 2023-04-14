6c71704422b178b30a8d3095630067d2c2a60a1b
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-14 11:30:57 +0300

    Registered viewsets with router

--- snippets/views.py
+++ snippets/views.py
@@ -1,23 +1,12 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import permissions
 from rest_framework import renderers
 from rest_framework import viewsets
-from rest_framework.decorators import api_view, action
+from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from .permissions import IsOwnerOrReadOnly
 from .serializers import SnippetSerializer, UserSerializer
 from .models import Snippet
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response(
-        {
-            'users': reverse('user-list', request=request, format=format),
-            'snippets': reverse('snippet-list', request=request, format=format)
-        }
-    )
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
